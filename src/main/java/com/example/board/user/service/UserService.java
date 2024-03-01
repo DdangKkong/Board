c16501d973ef58bd22c83fca8615a4fcb51da89e
DdangKkong <dnjsdn8@naver.com>
2024-03-01 13:58:36 +0900

    user sign in

--- src/main/java/com/example/board/user/service/UserService.java
+++ src/main/java/com/example/board/user/service/UserService.java
@@ -27,7 +27,7 @@ public class UserService {
 
   public Response signUp(Request request) {
 
-    // 로그인 아이디가 중복되는지 확인
+    // 아이디가 중복되는지 확인
     if (userRepository.existsByUserLoginId(request.getUserLoginId())) {
       throw new RuntimeException("Login ID already exists");
     }
@@ -53,7 +53,7 @@ public class UserService {
 
   public SignInDto.Response signIn(SignInDto.Request request) {
 
-    // 일치하는 회원 정보가 있는지 확인 ( userLoginId )
+    // 일치하는 아이디가 있는지 확인 ( userLoginId )
     if (!userRepository.existsByUserLoginId(request.getUserLoginId())) {
       throw new AppException(ErrorCode.USERLOGINID_NOTFOUND);
     }
